bdcd8d2b99d938d585c14feb5b3309844305ab51
Nick Dunn <nickdunn2@gmail.com>
2016-06-08 16:32:51 -0500

    conditionally show tweet button if text is entered

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,11 @@
 </div>
 
 <template id="tweets-template">
-	<input type="text" placeholder="What's happening?" v-model="newTweet" v-on:keyup.enter="addTweet"/>
+	<form @submit.prevent="addTweet">
+		<input type="text" placeholder="What's happening?" v-model="newTweet"/>
+		<button type="submit" v-show="newTweet"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Tweet</button>
+	</form>
+	
     <ul class="list-group">
         <li class="list-group-item" v-for="tweet in list">
             <strong>@{{ tweet.user.name }}</strong><br/>
